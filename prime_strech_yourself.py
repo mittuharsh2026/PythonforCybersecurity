4e40df19f4a2b0db41e402d0eeea0b23e17954aa
mittuharsh2026 <singh.harshit@student.greenriver.edu>
2026-05-04 11:13:18 -0700

    Add prime number checking functionality

---
+++ prime_strech_yourself.py
@@ -0,0 +1,15 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+number = int(input("What is the number? "))
+if is_prime(number):
+    print(f"{number} is prime")
+else:
+    print(f"{number} is NOT prime")
